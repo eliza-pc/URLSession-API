ea5df36bd4c09fb84d17820d1da3cb84b97d27bd
Eliza Carvalho <elizampcarvalho@gmail.com>
2018-11-19 09:20:38 -0400

    Initial Commit

---
+++ URLSessionTests/URLSessionTests.swift
@@ -0,0 +1,34 @@
+//
+//  URLSessionTests.swift
+//  URLSessionTests
+//
+//  Created by Eliza Maria Porto de Carvalho on 19/11/18.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import XCTest
+@testable import URLSession
+
+class URLSessionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
