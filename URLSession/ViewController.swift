7df2d4f067438fe90bf9748618df211ac557580c
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-04-22 09:49:47 -0400

    initial Commit 01

--- URLSession/ViewController.swift
+++ URLSession/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    func get_image(_ url_str:String, _imageView:UIImageView){
+        let url:URL = URL(string: url_str)!
+        let session = URLSession.shared
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if data != nil {
+                
+                let image = UIImage(data: data!)
+                if(image != nil){
+                    
+                    DispatchQueue.main.async(execute: { _imageView.image = image})
+                }
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
+    //test
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let mistica = "https://s.gravatar.com/avatar/10dceaef9932c7027fb2157cad710367?s=150"
+        
+       get_image(mistica, _imageView: image)
+        
     }
-
+    
 
 }
 
